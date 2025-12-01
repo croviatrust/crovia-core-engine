ce240aecc067eaa9c45157293bd51cf3b83ea7bf
Crovia <info@croviatrust.com>
2025-12-01 17:44:34 +0100

    Update cim_calibrate.py

--- model_side/cim_calibrate.py
+++ model_side/cim_calibrate.py
@@ -1,5 +1,18 @@
-#!/usr/bin/env python3
-# cim_calibrate.py - trova alpha per target avg top1 share (su FAISS scores)
+# Copyright 2025  Tarik En Nakhai
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 import argparse, json, numpy as np, faiss
 from cim_estimator import softmax_temp
 
@@ -41,3 +54,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
