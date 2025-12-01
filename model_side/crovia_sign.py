c7ed994281390358faf7e742ca766b833ca017aa
Crovia <info@croviatrust.com>
2025-12-01 17:45:18 +0100

    Update crovia_sign.py

--- model_side/crovia_sign.py
+++ model_side/crovia_sign.py
@@ -1,6 +1,21 @@
-# crovia_sign.py
+# Copyright 2025  Tarik En Nakhai
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 import hmac, hashlib, json, os
 
 def hmac_sign(obj: dict, secret: str) -> str:
     msg = json.dumps(obj, separators=(",", ":"), sort_keys=True).encode("utf-8")
     return hmac.new(secret.encode("utf-8"), msg, hashlib.sha256).hexdigest()
+
