767a848f062a5d75fdcdd51a0fe07926e718aba9
Crovia <info@croviatrust.com>
2025-12-01 17:45:59 +0100

    Update qa_receipts.py

--- model_side/qa_receipts.py
+++ model_side/qa_receipts.py
@@ -1,4 +1,18 @@
-#!/usr/bin/env python3
+# Copyright 2025  Tarik En Nakhai
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 import json, sys, math
 path = sys.argv[1]
 bad_sum = bad_order = bad_neg = 0
@@ -20,3 +34,4 @@ with open(path, "r", encoding="utf-8") as f:
         if ranks and ranks != sorted(ranks):
             bad_order += 1
 print(f"[QA] bad_sum={bad_sum}  bad_neg={bad_neg}  bad_order={bad_order}")
+
